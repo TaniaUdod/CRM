b037cb0a2bf9a342c9f76c54d10c544c9d996aab
TaniaUdod <taniaudod28@gmail.com>
2024-05-02 00:02:05 +0300

    added error handling components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import AddCompanyButton from './components/add-company-button';
-import StatusLabel, { Status } from './components/status-label';
+import MagicButton from './components/magic-button';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
+      <MagicButton />
     </main>
   );
 }
