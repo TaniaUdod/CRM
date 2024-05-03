ce6300f6d4296c7f5ca9050d2d711aa9119b2eb7
TaniaUdod <taniaudod28@gmail.com>
2024-05-04 00:33:29 +0300

    added using react-query

--- src/app/(admin)/companies/@toolbar/[id]/page.tsx
+++ src/app/(admin)/companies/@toolbar/[id]/page.tsx
@@ -1,5 +1,15 @@
-export interface PageProps {}
+import SearchInput from '@/app/components/search-input';
+import Toolbar from '@/app/components/toolbar';
+import AddPromotionButton from '@/app/components/add-promotion-button';
 
-export default function Page({}: PageProps) {
-  return null;
+export interface PageProps {
+  params: { id: string };
+}
+
+export default function Page({ params }: PageProps) {
+  return (
+    <Toolbar action={<AddPromotionButton companyId={params.id} />}>
+      <SearchInput />
+    </Toolbar>
+  );
 }
