2b5cbe3c008ac2f470cde24757f183879c72bf53
TaniaUdod <taniaudod28@gmail.com>
2024-05-04 23:35:15 +0300

    updated Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Sidebar from './components/sidebar';
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-5xl text-center mt-72 ml-60">Home page</h1>
+      <Sidebar />
     </main>
   );
 }
